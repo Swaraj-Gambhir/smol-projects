e1cb0bd5445ca3091d2854aa7396ad7e33d7848b
Swaraj-Gambhir <swarajpgambhir@gmail.com>
2024-01-05 12:27:51 +0530

    load-more and tree-view

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Accordian from './components/accordian/index.jsx';
 import RandomColor from './components/random-color/index.jsx';
 import StarRating from './components/star-rating/index.jsx';
 import ImageSlider from './components/image-slider/index.jsx';
+import LoadMoreData from './components/load-more-data/index.jsx';
+import TreeView from './components/tree-view/index.jsx';
+import {menus} from './components/tree-view/data.js';
 function App() {
  
 
@@ -11,7 +14,10 @@ function App() {
     {/* <Accordian /> */}
     {/* <RandomColor /> */}
     {/* < StarRating/> */}
-    <ImageSlider url={"https://picsum.photos/v2/list"} limit={"10"}/>
+    {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={"10"}/>
+     */}
+     {/* <LoadMoreData /> */}
+     <TreeView menus={menus}/>
     </>
   )
 }
